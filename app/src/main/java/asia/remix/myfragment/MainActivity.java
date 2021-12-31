8909f3fc1af6e3500b90eada4ddf59a49972868b
remix <http+github.com@remix.asia>
2021-12-31 09:26:43 +0900

    remove binding

--- app/src/main/java/asia/remix/myfragment/MainActivity.java
+++ app/src/main/java/asia/remix/myfragment/MainActivity.java
@@ -8,34 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import asia.remix.myfragment.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity{
 
 	private AppBarConfiguration appBarConfiguration;
-	private ActivityMainBinding binding;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 
-		binding = ActivityMainBinding.inflate( getLayoutInflater() );
-		setContentView( binding.getRoot() );
-
-		setSupportActionBar( binding.toolbar );
+		setContentView( R.layout.activity_main );
+		Toolbar toolbar = findViewById( R.id.toolbar );
+		setSupportActionBar( toolbar );
 
 		NavController navController = Navigation.findNavController( this, R.id.nav_host_fragment_content_main );
 		appBarConfiguration = new AppBarConfiguration.Builder( navController.getGraph() ).build();
 		NavigationUI.setupActionBarWithNavController( this, navController, appBarConfiguration );
 
-		binding.fab.setOnClickListener( new View.OnClickListener(){
+		FloatingActionButton fab = findViewById( R.id.fab );
+		fab.setOnClickListener( new View.OnClickListener(){
 			@Override
 			public void onClick( View view ){
 				Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG ).setAction( "Action", null ).show();
--- app/src/main/java/asia/remix/myfragment/SecondFragment.java
+++ app/src/main/java/asia/remix/myfragment/SecondFragment.java
@@ -4,28 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-import asia.remix.myfragment.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment{
 
-	private FragmentSecondBinding binding;
-
 	@Override
 	public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
-
-		binding = FragmentSecondBinding.inflate( inflater, container, false );
-		return binding.getRoot();
-
+		return inflater.inflate( R.layout.fragment_second, container, false );
 	}
 
 	public void onViewCreated( @NonNull View view, Bundle savedInstanceState ){
 		super.onViewCreated( view, savedInstanceState );
 
-		binding.buttonSecond.setOnClickListener( new View.OnClickListener(){
+		Button button = view.findViewById( R.id.button_second );
+		button.setOnClickListener( new View.OnClickListener(){
 			@Override
 			public void onClick( View view ){
 				NavHostFragment.findNavController( SecondFragment.this ).navigate( R.id.action_SecondFragment_to_FirstFragment );
@@ -36,7 +32,6 @@ public class SecondFragment extends Fragment{
 	@Override
 	public void onDestroyView(){
 		super.onDestroyView();
-		binding = null;
 	}
 
 }
