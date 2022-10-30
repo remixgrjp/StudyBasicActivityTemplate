17dc76d7e0a498ba8ee24999a073036882e88056
remix <http+github.com@remix.asia>
2022-10-30 13:25:27 +0900

    Android Studio Bumblebee | 2021.1.1 Patch 1 -> Android Studio Dolphin | 2021.3.1 Patch 1

--- app/src/main/java/asia/remix/myfragment/SecondFragment.java
+++ app/src/main/java/asia/remix/myfragment/SecondFragment.java
@@ -14,6 +14,7 @@ public class SecondFragment extends Fragment{
 
 	@Override
 	public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
+
 		return inflater.inflate( R.layout.fragment_second, container, false );
 	}
 
